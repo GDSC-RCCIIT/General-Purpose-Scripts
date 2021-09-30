690369b02680354afb4e3c9d44df0d9744f853b9
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-09-30 23:29:12 +0530

    url shortener script added
    Signed-off-by: Soham Banerjee <sohambanerjee4abc@hotmail.com>

---
+++ scripts/shorty.py
@@ -0,0 +1,12 @@
+
+import requests,os,sys
+print("Enter URL: ",end="")
+url = input()
+short = 'http://tinyurl.com/api-create.php?url='+url
+final = requests.get(short) #direct link is passed and shortened is returned here
+if(final.text=="Error"):
+    print("The provided Link is Not valid! Retry")
+else:
+#print (short)
+    print("Shortened Link: "+final.text)
+
