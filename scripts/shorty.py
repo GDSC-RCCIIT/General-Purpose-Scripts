0dbc89a85c6eabb00f4c788b9ac9d6ab41680022
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-09-30 23:29:43 +0530

    code formatted
    Signed-off-by: Soham Banerjee <sohambanerjee4abc@hotmail.com>

--- scripts/shorty.py
+++ scripts/shorty.py
@@ -1,12 +1,11 @@
+import requests, os, sys
 
-import requests,os,sys
-print("Enter URL: ",end="")
+print("Enter URL: ", end="")
 url = input()
-short = 'http://tinyurl.com/api-create.php?url='+url
-final = requests.get(short) #direct link is passed and shortened is returned here
-if(final.text=="Error"):
+short = "http://tinyurl.com/api-create.php?url=" + url
+final = requests.get(short)  # direct link is passed and shortened is returned here
+if final.text == "Error":
     print("The provided Link is Not valid! Retry")
 else:
-#print (short)
-    print("Shortened Link: "+final.text)
-
+    # print (short)
+    print("Shortened Link: " + final.text)
