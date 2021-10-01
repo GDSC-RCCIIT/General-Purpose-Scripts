ef182bd3ea1b9d4dd898d86b9273e19b257b02d8
Bikram <bkrmprkt@gmail.com>
2021-10-01 20:27:05 +0530

    update change

--- scripts/NewsDown/main.py
+++ scripts/NewsDown/main.py
@@ -1,11 +1,9 @@
 import requests
-import datetime
-import img2pdf
 from bs4 import BeautifulSoup
-from tkinter import ttk, filedialog
-from tkinter import *
+import img2pdf
+from tkinter import ttk, Tk, LabelFrame, Frame, filedialog
 from tkcalendar import DateEntry
-
+import datetime
 
 file_name = ""
 finaltime = ""
