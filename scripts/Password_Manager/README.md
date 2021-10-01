5f576bc4872ca4ffdd3d451bc09f24250ce22f0b
Sawan Bhattacharya <shymal.sawan@gmail.com>
2021-10-01 16:27:23 +0530

    Add Password Manager encryption

--- scripts/Password_Manager/README.md
+++ scripts/Password_Manager/README.md
@@ -1,3 +1,6 @@
 <h1 align="center"> Simple Password Manager </h1>
 
-Simple Password manager to store password securely
+Simple Password manager to store password securely 
+## Setup 
+- Run `pip3 install -r requirements.txt` to install dependency 
+- To run the file `python3 main.py`
--- scripts/Password_Manager/main.py
+++ scripts/Password_Manager/main.py
@@ -1,18 +1,54 @@
 import random
 import base64
 import pickle
+from Crypto.Cipher import AES
+import hashlib
 
 
-def AutoGen():
+def AutoGen(number):
     passwd = ""
-    for i in range(12):
+    for i in range(number):
         passwd += chr(random.randint(33, 126))
     return passwd
 
 
+def encrypt_file(message):
+
+    password = "password".encode()
+
+    key = hashlib.sha256(password).digest()
+
+    mode = AES.MODE_CBC
+
+    IV = "This is an IV456"
+
+    def pad_message(message):
+        while len(message) % 16 != 0:
+            message = message + " "
+        return message
+
+    cipher = AES.new(key, mode, IV)
+    # message = "my meassage"
+    pad_message = pad_message(message)
+
+    encrypt_message = cipher.encrypt(pad_message)
+    return encrypt_message
+
+
+def decrypt_file(code):
+    password = b"password"
+    key = hashlib.sha256(password).digest()
+    mode = AES.MODE_CBC
+    IV = "This is an IV456"
+    cipher = AES.new(key, mode, IV)
+
+    decrypt_message = str(cipher.decrypt(code)).lstrip("b'").rstrip("'")
+    return decrypt_message
+
+
 def Add_Passwd(site, user, passwd):
     file = open("scripts/Password_Manager/data.dat", "ab")
-    enc_passwd = base64.b64encode(passwd.encode("utf-8"))
+    enc_passwd = encrypt_file(passwd)
     enc_user = base64.b64encode(user.encode("utf-8"))
     enc_site = base64.b64encode(site.encode("utf-8"))
     E1 = {"site": enc_site, "user": enc_user, "passwd": enc_passwd}
@@ -35,11 +71,7 @@ def Print_Passwd(site):
                     .lstrip("b'")
                     .rstrip("'")
                 )
-                passwd = (
-                    str(base64.b64decode(x["passwd"].decode("utf-8")))
-                    .lstrip("b'")
-                    .rstrip("'")
-                )
+                passwd = decrypt_file(x["passwd"])
                 return user, passwd
     except:
         pass
@@ -52,17 +84,23 @@ while True:
     to quit type `exit` """
     print(x)
     command = input(">>")
+
     if command == "1":
-        print(AutoGen())
+        user = int(input("Enter size of password (recommended 12) >>"))
+        print(AutoGen(user))
+
     elif command == "2":
         print("enter site, user & password seperated by space ")
         data = list(input("here: ").split(" "))
         Add_Passwd(data[0], data[1], data[2])
+
     elif command == "3":
         data = input("Enter site name: ")
         user, passwd = Print_Passwd(data)
         print(user, "|", passwd)
+
     elif command == "exit":
         break
+
     else:
         print("No such command exist")
