688a87f4ab3db33d809f3682bbe5c833a87590c3
Kushagra Gupta <kushagra357@gmail.com>
2021-10-02 23:02:55 +0530

    Create TextTohandwritten.py

---
+++ scripts/Text to handwritten/TextTohandwritten.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.text_to_handwriting("Open source is a term that originally referred to open source software (OSS). Open source software is code that is designed to be publicly accessible—anyone can see, modify, and distribute the code as they see fit.",rgb=(0,0,225))
