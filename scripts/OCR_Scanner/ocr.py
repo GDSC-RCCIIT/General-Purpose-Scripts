c3595b37103e48dfd558a85a948469d5a84db5f6
Harshil Jani <79367883+Harshil-Jani@users.noreply.github.com>
2021-10-03 01:15:02 +0530

    Update ocr.py

--- scripts/OCR_Scanner/ocr.py
+++ scripts/OCR_Scanner/ocr.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import pytesseract
 import numpy as np
-
+# Incase If this link don't work try giving address path on your local machine while running the script
 filename = 'https://github.com/GDSC-RCCIIT/General-Purpose-Scripts/blob/main/scripts/OCR_Scanner/image_01.png'
 img = np.array(Image.open(filename))
 text = pytesseract.image_to_string(img)
