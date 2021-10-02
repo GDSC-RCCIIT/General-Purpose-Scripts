b21b0944d7ec45b55b3a31a11562cd8c2d3753bb
Aniket Artani <aniketartani25@gmail.com>
2021-10-03 01:55:54 +0530

    Update video_to_gif.py

--- scripts/video_to_gif/video_to_gif.py
+++ scripts/video_to_gif/video_to_gif.py
@@ -1,5 +1,5 @@
 from moviepy.editor import *
 
-clip = (VideoFileClip("filename.extension(like mp4)").subclip((0.2),(0.5)))
-clip.write_gif("output.gif")
+clip = (VideoFileClip("filename.extension(like mp4)").subclip((0.2),(0.5))) 
+clip.write_gif("output.gif")  #name of the output file will be output.gif
 print("completed")
