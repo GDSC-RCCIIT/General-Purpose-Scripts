be73e84275a079cd2bd4a47faf6cf06c8ca4333c
rob <hassenmayerr@gmail.com>
2021-10-03 02:03:00 -0400

    used black on script

--- scripts/CLI_pie_chart_maker/cli_pychart.py
+++ scripts/CLI_pie_chart_maker/cli_pychart.py
@@ -2,7 +2,9 @@
 from matplotlib.pyplot import pie, show
 
 
-print("Please write a list of chart wedge names/titles seperated with a comma and space")
+print(
+    "Please write a list of chart wedge names/titles seperated with a comma and space"
+)
 names = input("Names:\n")
 name_list = names.split(", ")
 print("Please write the values to each name in the same order and format")
@@ -11,14 +13,17 @@ value_list = values.split(", ")
 
 final_val = []
 for n in value_list:
-	final_val.append(float(n))
+    final_val.append(float(n))
 
 perc_list = []
 for i in final_val:
-	p = i/(sum(final_val)) * 100
-	perc_list.append(p)
+    p = i / (sum(final_val)) * 100
+    perc_list.append(p)
 
-pie(x = perc_list, labels = name_list, radius = 1.3,)
+pie(
+    x=perc_list,
+    labels=name_list,
+    radius=1.3,
+)
 
 show()
-
