07de2babcdcf0bdd7c10e64d0fd883bb8d9c9384
imsushant12 <sushantgaurav57@gmail.com>
2021-10-04 00:57:56 +0530

    Added Notification Script

---
+++ scripts/Notification_Reminder/notification.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+while True:
+    notification.notify(
+        title="Hey there!",
+        message="This is a pop-up reminder, take a break. Bye!",
+ 		# app_icon = "<path of the icon-image>",
+        timeout=10
+
+    	)
+    time.sleep(1500) # time is seconds
