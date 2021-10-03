e03d3e9db30a9f3e1356bb126adf5915397b613b
Harshil Jani <79367883+Harshil-Jani@users.noreply.github.com>
2021-10-03 11:17:10 +0530

    Update ocr.py

--- scripts/OCR_Scanner/ocr.py
+++ scripts/OCR_Scanner/ocr.py
@@ -2,15 +2,20 @@ from PIL import Image
 import pytesseract
 import numpy as np
 
-#Input your Local Machine Address in filename where you have stored your image.
-filename = 'https://github.com/Harshil-Jani/General-Purpose-Scripts/blob/OCR-Script/scripts/OCR_Scanner/image_01.png'
+# Input your Local Machine Address in filename where you have stored your image.
+path = input("Enter your Image Path Address : ")
+filename = path
 img = np.array(Image.open(filename))
 text = pytesseract.image_to_string(img)
 # print(text)
 content = text
 
-#Input your Local Machine Address to open ocr.txt file
-with open("https://github.com/Harshil-Jani/General-Purpose-Scripts/blob/OCR-Script/scripts/OCR_Scanner/ocr.txt","w") as f:
-   content = f.write(content)
-   f.close()
-   
+# Input your Local Machine Address to open ocr.txt file
+textfile = input("Enter the file path of text file : ")
+with open(
+    textfile,
+    "w",
+) as f:
+    content = f.write(content)
+    f.close()
+`
