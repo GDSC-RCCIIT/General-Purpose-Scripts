725c9dbbe138308e7a4fba7fa05e6e0d97a11451
Dr-blue-cyber <surajbhanarkar08@gmail.com>
2021-10-05 07:49:09 +0530

    run the black command and fixed issue

--- scripts/Secured chat machine/main.py
+++ scripts/Secured chat machine/main.py
@@ -19,9 +19,14 @@ f1.pack(side=LEFT)
 
 # UI BUILDING
 
-lblInfo = Label(Tops, font=('helvitica', 50, 'bold'),
-                text="Secured Chat Machine with \n End-to_End Encryption",
-                fg="Black", bd=10, anchor='w')
+lblInfo = Label(
+    Tops,
+    font=("helvitica", 50, "bold"),
+    text="Secured Chat Machine with \n End-to_End Encryption",
+    fg="Black",
+    bd=10,
+    anchor="w",
+)
 
 lblInfo.grid(row=0, column=0)
 
@@ -31,41 +36,70 @@ key = StringVar()
 mode = StringVar()
 Result = StringVar()
 
-lblMsg = Label(f1, font=('arial', 16, 'bold'),
-               text=" MESSAGE ", bd=16, anchor="w")
+lblMsg = Label(f1, font=("arial", 16, "bold"), text=" MESSAGE ", bd=16, anchor="w")
 lblInfo.grid(row=1, column=0)
 
-txtMsg = Entry(f1, font=('arial', 16, 'bold'),
-               textvariable=Msg, bd=10, insertwidth=4,
-               bg="Powder blue", justify='center')
+txtMsg = Entry(
+    f1,
+    font=("arial", 16, "bold"),
+    textvariable=Msg,
+    bd=10,
+    insertwidth=4,
+    bg="Powder blue",
+    justify="center",
+)
 txtMsg.grid(row=1, column=1)
 
-lblkey = Label(f1, font=('arial', 16, 'bold'),
-               text="Key (only interger)", bd=16, anchor="w")
+lblkey = Label(
+    f1, font=("arial", 16, "bold"), text="Key (only interger)", bd=16, anchor="w"
+)
 lblkey.grid(row=2, column=0)
 
-txtkey = Entry(f1, font=('arial', 16, 'bold'),
-               textvariable=key, bd=10, insertwidth=4,
-               bg="powder blue", justify='center')
+txtkey = Entry(
+    f1,
+    font=("arial", 16, "bold"),
+    textvariable=key,
+    bd=10,
+    insertwidth=4,
+    bg="powder blue",
+    justify="center",
+)
 txtkey.grid(row=2, column=1)
 
-lblmode = Label(f1, font=('arial', 16, 'bold'),
-                text="MODE(e for encrypt, d for decrypt)",
-                bd=10, anchor="w")
+lblmode = Label(
+    f1,
+    font=("arial", 16, "bold"),
+    text="MODE(e for encrypt, d for decrypt)",
+    bd=10,
+    anchor="w",
+)
 lblmode.grid(row=3, column=0)
 
-txtmode = Entry(f1, font=('arial', 16, 'bold'),
-                textvariable=mode, bd=10, insertwidth=4, bg="Powder blue",
-                justify='center')
+txtmode = Entry(
+    f1,
+    font=("arial", 16, "bold"),
+    textvariable=mode,
+    bd=10,
+    insertwidth=4,
+    bg="Powder blue",
+    justify="center",
+)
 txtmode.grid(row=3, column=1)
 
-lblResult = Label(f1, font=('arial', 16, 'bold'),
-                  text="The Result-", bd=16, justify='center')
+lblResult = Label(
+    f1, font=("arial", 16, "bold"), text="The Result-", bd=16, justify="center"
+)
 lblResult.grid(row=2, column=2)
 
-txtResult = Entry(f1, font=('arial', 16, 'bold'),
-                  textvariable=Result, bd=10, insertwidth=4,
-                  bg="white", justify='center')
+txtResult = Entry(
+    f1,
+    font=("arial", 16, "bold"),
+    textvariable=Result,
+    bd=10,
+    insertwidth=4,
+    bg="white",
+    justify="center",
+)
 
 txtResult.grid(row=2, column=3)
 
@@ -73,13 +107,13 @@ txtResult.grid(row=2, column=3)
 # VIFGENERE CIPHER
 # FUNCTION TO ENCODE
 
+
 def encode(key, msg):
     enc = []
 
     for i in range(len(msg)):
         key_c = key[i % len(key)]
-        enc_c = chr((ord(msg[i]) +
-                     ord(key_c)) % 256)
+        enc_c = chr((ord(msg[i]) + ord(key_c)) % 256)
         enc.append(enc_c)
         print("enc:", enc)
     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
@@ -92,8 +126,7 @@ def decode(key, enc):
     enc = base64.urlsafe_b64decode(enc).decode()
     for i in range(len(enc)):
         key_c = key[i % len(key)]
-        dec_c = chr((256 + ord(enc[i]) -
-                     ord(key_c)) % 256)
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
         dec.append(dec_c)
     return "".join(dec)
 
@@ -116,27 +149,51 @@ def Results():
     k = key.get()
     m = mode.get()
 
-    if (m == 'e'):
+    if m == "e":
         Result.set(encode(k, msg))
     else:
         Result.set(decode(k, msg))
 
 
 # SHOW MESSAGE BTN
-btnTotal = Button(f1, padx=16, pady=8, bd=16, fg="black",
-                  font=('arial', 16, 'bold'), width=10,
-                  text="Show Message", bg="powder blue",
-                  command=Results).grid(row=7, column=1)
+btnTotal = Button(
+    f1,
+    padx=16,
+    pady=8,
+    bd=16,
+    fg="black",
+    font=("arial", 16, "bold"),
+    width=10,
+    text="Show Message",
+    bg="powder blue",
+    command=Results,
+).grid(row=7, column=1)
 
 # RESET BTN
-btnReset = Button(f1, padx=16, pady=8, bd=16, fg="black",
-                  font=('arial', 16, 'bold'), width=10,
-                  text="Reset", bg="green",
-                  command=Reset).grid(row=7, column=2)
+btnReset = Button(
+    f1,
+    padx=16,
+    pady=8,
+    bd=16,
+    fg="black",
+    font=("arial", 16, "bold"),
+    width=10,
+    text="Reset",
+    bg="green",
+    command=Reset,
+).grid(row=7, column=2)
 
 # Exit BTN
-btnExit = Button(f1, padx=16, pady=8, bd=16, fg="black",
-                 font=('arial', 16, 'bold'), width=10,
-                 text="Exit", bg="red",
-                 command=qExit).grid(row=7, column=3)
+btnExit = Button(
+    f1,
+    padx=16,
+    pady=8,
+    bd=16,
+    fg="black",
+    font=("arial", 16, "bold"),
+    width=10,
+    text="Exit",
+    bg="red",
+    command=qExit,
+).grid(row=7, column=3)
 root.mainloop()
