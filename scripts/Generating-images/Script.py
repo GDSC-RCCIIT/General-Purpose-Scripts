30067f953e1e04ddbcd139141253175b1d99885b
Sayak Banerjee <46437624+Sayak09@users.noreply.github.com>
2021-10-06 14:15:21 +0530

    Add files via upload
    Generating Images

---
+++ scripts/Generating-images/Script.py
@@ -0,0 +1,12 @@
+# Opens the Video file
+cap= cv2.VideoCapture('C:/New/Videos/Play.mp4')
+i=0
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite('kang'+str(i)+'.jpg',frame)
+    i+=1
+ 
+cap.release()
+cv2.destroyAllWindows()
