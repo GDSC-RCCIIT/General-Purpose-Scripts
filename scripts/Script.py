ffdb09e826c3636207de64be9e46ba56e1cc844b
Sayak Banerjee <46437624+Sayak09@users.noreply.github.com>
2021-10-06 14:23:49 +0530

    Add files via upload

---
+++ scripts/Script.py
@@ -0,0 +1,11 @@
+# Opens the Video file
+cap= cv2.VideoCapture('C:/New/Videos/Play.mp4')
+i=0
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite('kang'+str(i)+'.jpg',frame)
+    i+=1
+ 
+cap.release()
