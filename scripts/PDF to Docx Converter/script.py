63e20e67f12cb974df43bff88267c1c1a13df256
Pragya Khanna <75221153+pragyakhanna11@users.noreply.github.com>
2021-10-08 11:36:02 +0530

    Update script.py

--- scripts/PDF to Docx Converter/script.py
+++ scripts/PDF to Docx Converter/script.py
@@ -8,7 +8,7 @@ word.visible = 0
 pdfs_path = ""  # folder where the .pdf files are stored
 for i, doc in enumerate(glob.iglob(pdfs_path + "*.pdf")):
     print(doc)
-    filename = doc.split('\\')[-1]
+    filename = doc.split("\\")[-1]
     in_file = os.path.abspath(doc)
     print(in_file)
     wb = word.Documents.Open(in_file)
