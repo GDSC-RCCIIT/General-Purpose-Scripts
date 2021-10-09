23b5c33020c9e765966454297acf3f490e816fce
MEGHA SINGH <67351219+megha377@users.noreply.github.com>
2021-10-10 01:59:51 +0530

    Update animeRecommendation2.py

--- scripts/AnimeRecommendationModel/animeRecommendation2.py
+++ scripts/AnimeRecommendationModel/animeRecommendation2.py
@@ -7,57 +7,190 @@ import warnings
 # ignores all warnings we get for better user-side experience
 warnings.filterwarnings("ignore")
 
-# fetching the required columns from anime dataset
-df = (
-    pd.read_csv("animes.csv")
-    .loc[:, ["uid", "title", "members", "synopsis"]]
-    .rename(columns={"uid": "anime_id"})
-)
-df["title"] = df["title"].str.lower()
-df
+#reading the datasets
+anime_data=pd.read_csv('anime.csv')
+rating_data=pd.read_csv('rating.csv')
 
-# cleaning the dataset
-df["synopsis"] = df["synopsis"].str.replace("\r", "")
-df["synopsis"] = df["synopsis"].str.replace("\n", "")
-df["synopsis"] = df["synopsis"].fillna("")
+## print shape of dataset with rows and columns and information - anime_data
+print ("The shape of the  data is (row, column):"+ str(anime_data.shape))
+print (anime_data.info())
+## print shape of dataset with rows and columns and information- user rating
+print ("The shape of the  data is (row, column):"+ str(rating_data.shape))
+print (rating_data.info())
 
-for i in range(0, 19311):
-    df["synopsis"][i] = df["synopsis"][i].split("  [")[0]
+#We are merging our csv's based on anime_id from both datasets and later we are renaming the columns
+anime_fulldata=pd.merge(anime_data,rating_data,on='anime_id',suffixes= ['', '_user'])
+anime_fulldata = anime_fulldata.rename(columns={'name': 'anime_title', 'rating_user': 'user_rating'})
+anime_fulldata.head()
 
-
-# saving the cleaned dataset
-df.to_csv("anime_dataset.csv")
-
-# fetching the cleaned dataset
-df = pd.read_csv("anime_dataset.csv").iloc[:5000, 1:]
-df = df.sort_values(by="anime_id").drop_duplicates(subset=["title"])
-
-# importing the Count Vectorisation class and making a vectoriser object
-from sklearn.feature_extraction.text import CountVectorizer
-
-cv = CountVectorizer(max_features=5000, stop_words="english")
-
-# making the text vectoriser bag of words
-tv = cv.fit_transform(df["synopsis"].astype("U")).toarray()
-
-# now importing cosine similarity from sklearn and applying it on the bag of words
-from sklearn.metrics.pairwise import cosine_similarity
-
-cos_sim = cosine_similarity(tv)
-cos_sim
-
-# THE MAIN FUNCTION WHICH RECOMMENDS 10 ANIMES CLOSEST TO THE GIVEN ANIME BASED ON THE SYNOPSIS
-def recommender(anime):
-    index = df[df["title"] == anime].index[0]
-    distances = sorted(
-        list(enumerate(cos_sim[index])), reverse=True, key=lambda x: x[1]
+# Creating a dataframe for rating counts
+combine_anime_rating = anime_fulldata.dropna(axis = 0, subset = ['anime_title'])
+anime_ratingCount = (combine_anime_rating.
+     groupby(by = ['anime_title'])['user_rating'].
+     count().
+     reset_index().rename(columns = {'rating': 'totalRatingCount'})
+    [['anime_title', 'user_rating']]
     )
-    for i in distances[1:10]:
-        print(df.iloc[i[0]].title)
 
+# Plotting the bar plot for top 10 anime as per rating 
+top10_animerating=anime_ratingCount[['anime_title', 'user_rating']].sort_values(by = 'user_rating',ascending = False).head(10)
+ax=sns.barplot(x="anime_title", y="user_rating", data=top10_animerating, palette="Dark2")
+ax.set_xticklabels(ax.get_xticklabels(), fontsize=11, rotation=40, ha="right")
+ax.set_title('Top 10 Anime based on rating counts',fontsize = 22)
+ax.set_xlabel('Anime',fontsize = 20) 
+ax.set_ylabel('User Rating count', fontsize = 20)
 
-# FEW TEST RUNS
-recommender("made in abyss")
-recommender("dragon ball")
-recommender("death note")
-recommender("one piece")
+#Creating a copy and dropping the duplicates
+duplicate_anime=anime_fulldata.copy()
+duplicate_anime.drop_duplicates(subset ="anime_title", 
+                     keep = 'first', inplace = True)
+#Plotting bar plot               
+top10_animemembers=duplicate_anime[['anime_title', 'members']].sort_values(by = 'members',ascending = False).head(10)
+ax=sns.barplot(x="anime_title", y="members", data=top10_animemembers, palette="gnuplot2")
+ax.set_xticklabels(ax.get_xticklabels(), fontsize=11, rotation=40, ha="right")
+ax.set_title('Top 10 Anime based on members',fontsize = 22)
+ax.set_xlabel('Anime',fontsize = 20) 
+ax.set_ylabel('Community Size', fontsize = 20)
+
+#Distribution of ratings
+plt.figure(figsize = (15, 7))
+plt.subplot(1,2,1)
+anime_fulldata['rating'].hist(bins=70)
+plt.title("Rating of websites")
+plt.subplot(1,2,2)
+anime_fulldata['user_rating'].hist(bins=70)
+plt.title("Rating of users")
+
+import plotly.graph_objects as go
+labels = anime_fulldata['type'].value_counts().index
+values = anime_fulldata['type'].value_counts().values
+colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen']
+fig = go.Figure(data=[go.Pie(labels=labels,
+                             values=values)])
+fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
+                  marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+
+fig.update_layout(
+    title={
+        'text': "Medium of Streaming",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+
+fig.show()
+
+#GENRE WORD CLOUD
+#Sharingan copy: https://www.kaggle.com/indralin/try-content-based-and-collaborative-filtering
+nonull_anime=anime_fulldata.copy()
+nonull_anime.dropna(inplace=True)
+from collections import defaultdict
+
+all_genres = defaultdict(int)
+
+for genres in nonull_anime['genre']:
+    for genre in genres.split(','):
+        all_genres[genre.strip()] += 1
+        
+from wordcloud import WordCloud
+
+genres_cloud = WordCloud(width=800, height=400, background_color='white', colormap='gnuplot').generate_from_frequencies(all_genres)
+plt.imshow(genres_cloud, interpolation='bilinear')
+plt.axis('off')
+
+#Replacing -1 with NaN in user_rating column
+anime_feature=anime_fulldata.copy()
+anime_feature["user_rating"].replace({-1: np.nan}, inplace=True)
+anime_feature.head()
+
+#dropping all the null values as it aids nothing
+anime_feature = anime_feature.dropna(axis = 0, how ='any') 
+anime_feature.isnull().sum()
+
+anime_feature['user_id'].value_counts()
+
+counts = anime_feature['user_id'].value_counts()
+anime_feature = anime_feature[anime_feature['user_id'].isin(counts[counts >= 200].index)]
+
+anime_pivot=anime_feature.pivot_table(index='anime_title',columns='user_id',values='user_rating').fillna(0)
+anime_pivot.head()
+
+from scipy.sparse import csr_matrix
+#Creating a sparse matrix
+anime_matrix = csr_matrix(anime_pivot.values)
+
+from sklearn.neighbors import NearestNeighbors
+
+#Fitting the model
+model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
+model_knn.fit(anime_matrix)
+
+query_index = np.random.choice(anime_pivot.shape[0])
+#print(query_index)
+distances, indices = model_knn.kneighbors(anime_pivot.iloc[query_index,:].values.reshape(1, -1), n_neighbors = 6)
+
+#Testing collaborative recommendation
+for i in range(0, len(distances.flatten())):
+    if i == 0:
+        print('Recommendations for {0}:\n'.format(anime_pivot.index[query_index]))
+    else:
+        print('{0}: {1}, with distance of {2}:'.format(i, anime_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
+        
+        
+#cleaning anime_title
+Sharingan copy:https://www.kaggle.com/indralin/try-content-based-and-collaborative-filtering
+import re
+def text_cleaning(text):
+    text = re.sub(r'&quot;', '', text)
+    text = re.sub(r'.hack//', '', text)
+    text = re.sub(r'&#039;', '', text)
+    text = re.sub(r'A&#039;s', '', text)
+    text = re.sub(r'I&#039;', 'I\'', text)
+    text = re.sub(r'&amp;', 'and', text)
+    
+    return text
+
+anime_data['name'] = anime_data['name'].apply(text_cleaning)
+
+#Term Frequency (TF) and Inverse Document Frequency (IDF)
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#getting tfidf
+tfv = TfidfVectorizer(min_df=3,  max_features=None, 
+            strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
+            ngram_range=(1, 3),
+            stop_words = 'english')
+
+# Filling NaNs with empty string
+anime_data['genre'] = anime_data['genre'].fillna('')
+genres_str = anime_data['genre'].str.split(',').astype(str)
+tfv_matrix = tfv.fit_transform(genres_str)
+
+from sklearn.metrics.pairwise import sigmoid_kernel
+
+# Compute the sigmoid kernel
+sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
+
+#getting the indices of anime title
+indices = pd.Series(anime_data.index, index=anime_data['name']).drop_duplicates()
+
+#content based recommendation function
+def give_rec(title, sig=sig):
+    # Get the index corresponding to original_title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores 
+    sig_scores = list(enumerate(sig[idx]))
+
+    # Sort the movies 
+    sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
+
+    # Scores of the 10 most similar movies
+    sig_scores = sig_scores[1:11]
+
+    # Movie indices
+    anime_indices = [i[0] for i in sig_scores]
+
+    # Top 10 most similar movies
+    return pd.DataFrame({'Anime name': anime_data['name'].iloc[anime_indices].values,
+                                 'Rating': anime_data['rating'].iloc[anime_indices].values})
