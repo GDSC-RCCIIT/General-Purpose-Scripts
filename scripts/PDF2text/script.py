c33e15cb799a9e2e29dbbeff16948aa7b04bf25e
pdesai878 <63565627+pdesai878@users.noreply.github.com>
2021-10-14 01:58:00 +0530

    formatted script.py with black

--- scripts/PDF2text/script.py
+++ scripts/PDF2text/script.py
@@ -2,15 +2,14 @@ import os
 import pdftotext
 
 
-# pdf_path = input("Enter the path of the pdf file : ")
-pdf_path='sample.pdf'
+pdf_path = input("Enter the path of the pdf file : ")
 
-# assert os.path.exists(pdf_path), "this pdf file doesn't exist"
+assert os.path.exists(pdf_path), "this pdf file doesn't exist"
 
-with open(pdf_path, 'rb') as f_r:
+with open(pdf_path, "rb") as f_r:
     pdf_pages = pdftotext.PDF(f_r)
 
 for i, page in enumerate(pdf_pages):
-    print('Page {}'.format(i))
+    print("Page {}".format(i))
     print(page)
-    print('*'*100)
+    print("*" * 100)
