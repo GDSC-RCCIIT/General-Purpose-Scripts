0210c95bf88c9eb19e34e259b3c83feadcb00d46
Prabesh Shrestha <creatorworld329@gmail.com>
2021-10-19 18:29:47 +0545

    Motivator

--- scripts/motivator/main.py
+++ scripts/motivator/main.py
@@ -1,42 +1,42 @@
 from gtts import gTTS
 from playsound import playsound
 from os import remove
-from requests import get  
+from requests import get
+
+
 def rand_quotes():
-    res = get('https://zenquotes.io/api/random').json()[0]
+    res = get("https://zenquotes.io/api/random").json()[0]
     return f'{res["q"]} by {res["a"]}'
-    
+
+
 def day_quotes():
-    res = get('https://zenquotes.io/api/today').json()[0]
+    res = get("https://zenquotes.io/api/today").json()[0]
     return f'{res["q"]} by {res["a"]}'
 
 
 def say(text):
-    gTTS(text=text, lang='en', slow=False).save('temp.mp3')
-    playsound('temp.mp3')
-    remove('temp.mp3')
+    gTTS(text=text, lang="en", slow=False).save("temp.mp3")
+    playsound("temp.mp3")
+    remove("temp.mp3")
 
 
-if __name__ == '__main__':
-    print('type `help` for more information')
+if __name__ == "__main__":
+    print("type `help` for more information")
     while True:
-        inp = input('==>')
-        if inp == '?' or inp == 'help':
-            print(''' Commands: 
+        inp = input("==>")
+        if inp == "?" or inp == "help":
+            print(
+                """ Commands: 
             random
             today
             exit
-            ''')
-        if inp == 'random':
+            """
+            )
+        if inp == "random":
             say(rand_quotes())
-        if inp == 'today':
+        if inp == "today":
             say(day_quotes())
-        if inp == 'exit':
-            print('bye bye :) ')
+        if inp == "exit":
+            print("bye bye :) ")
             break
 
-    # say(rand_quotes())
-    # day_quotes()
-    
-    # say('helloworld')
-    # print(rand_quotes())
