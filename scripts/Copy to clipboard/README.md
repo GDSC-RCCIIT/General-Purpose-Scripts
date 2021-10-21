44d036e5f3f8fb09112078003d72a820ab7f7fd7
Adipta Santra <adipta1010@gmail.com>
2021-10-21 20:44:37 +0530

    Added script to copy to clipboard from text file and its readme file

---
+++ scripts/Copy to clipboard/README.md
@@ -0,0 +1,16 @@
+## How to run
+
+1. Clone the repository
+   ```
+   git clone git@github.com:GDSC-RCCIIT/General-Purpose-Scripts.git
+   ```
+2. Navigate to `Copy to clipboard` directory
+   ```
+   cd General-Purpose-Scripts/scripts/Copy to clipboard
+   ```
+3. Run the script to copy text from the desired file to your clipboard
+   ```
+   python clipboard.py <file_name>
+   ```
+   ### Note
+   Replace <file_name> with the name of the file present in the same directory as the script or replace it with the path of the file.
---
+++ scripts/Copy to clipboard/clipboard.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import platform
+import subprocess
+
+# Seeing if the file exists
+if os.path.exists(sys.argv[1]):
+    f = open(sys.argv[1], "r")
+    f_contents = f.read()
+    f.close()
+else:
+    print(
+        "Usage: python clipboard.py file_name\n      OR\n       python clipboard.py 'path_of_the_file'"
+    )
+    exit(1)
+
+whatos = platform.system()
+
+if whatos == "Darwin":
+    subprocess.run("pbcopy", universal_newlines=True, input=f_contents)
+    print("success: copied to clipboard")
+elif whatos == "Windows":
+    subprocess.run("clip", universal_newlines=True, input=f_contents)
+    print("Success! Copied to clipboard.")
+else:
+    print("Failed! Clipboard not supported.")
