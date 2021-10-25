2938cd9752a9087121e36f349b9133ffc0ac0481
Lovish Prabhakar <2020010006574@gndu.ac.in>
2021-10-25 22:18:36 +0530

    Add files via upload

---
+++ scripts/Text-to-audio-covertor/txt_to_aud.py
@@ -0,0 +1,64 @@
+import os
+from gtts import gTTS
+
+# Beginning:
+resp = int(
+    input(
+        """Convert text to speech\n\nPress:\n\t<1> To enter text by typing\n\t<2> To scan text from a text file\n >> """
+    )
+)
+
+if resp == 1:
+    # The text that you want to convert to audio
+
+    mytext = input("Enter text here:\n")
+    language = "en"
+    filteredtext = ""
+    for i in mytext:
+        if i == " ":
+            continue
+        filteredtext += i
+    # Language in which you want to convert
+
+    """ Passing the text and language to the engine, here we have marked slow=False. This tells the module that the converted audio should have a high speed."""
+
+    # Checking whether the input text is correct or not.
+
+    if filteredtext.isalpha():
+        myobj = gTTS(text=mytext, lang=language, slow=False)
+    else:
+        myobj = gTTS("Please Enter correct value", lang=language, slow=False)
+
+    # Saving the converted audio in a mp3 file named convertedaud
+
+    myobj.save("voice.mp3")
+
+    # Playing the converted file
+    os.system("voice.mp3")
+
+elif resp == 2:
+    # You will need a text file named test.txt to play, put it in the current dir.
+
+    FLIST = open("test.txt", "r").read().replace("\n", " ")
+
+    print("please wait...processing")
+
+    filteredtext = ""
+    for i in FLIST:
+        if i == " ":
+            continue
+        filteredtext += i
+
+    if filteredtext.isalpha():
+        TTS = gTTS(text=str(FLIST), lang="en-uk", slow=False)
+    else:
+        TTS = gTTS("Please Enter correct value", lang="en-uk", slow=False)
+
+    # Save to mp3 in current dir.
+
+    TTS.save("voice.mp3")
+
+    # Plays the mp3 using the default app on your system that is linked to mp3s.
+
+    print(FLIST)
+    os.system("start voice.mp3")
