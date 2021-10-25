fdf2de67e93ba93e7c916fd7b5ea8b6b28998503
Adipta Santra <adipta1010@gmail.com>
2021-10-25 12:32:09 +0530

    Added support for numeric digits and decimal in script

--- scripts/NATO Alphabet/script.py
+++ scripts/NATO Alphabet/script.py
@@ -30,6 +30,17 @@ nato_alphabet = {
     "x": "x-ray",
     "y": "yankee",
     "z": "zulu",
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "niner",
+    ".": "decimal",
 }
 
 try:
