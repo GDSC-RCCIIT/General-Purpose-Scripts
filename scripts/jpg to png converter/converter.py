32ff54a8c1356714ab5be967f6dc130ea4910d79
Raunak Kumar <52267072+Raunakkumarr@users.noreply.github.com>
2021-10-27 12:29:20 +0545

    Update converter.py

--- scripts/jpg to png converter/converter.py
+++ scripts/jpg to png converter/converter.py
@@ -1,13 +1,14 @@
+#Import Libraries
 import os
 from PIL import Image
 
-
+#Converter Function
 def converter():
-  folderPath = 'D:\\crush\\'
+  folderPath = 'D:\\crush\\' #Location of the folder with jpg files
   fileSequence = 1
   for filename in os.listdir(folderPath):
     im1 = Image.open(folderPath+filename)
-    im1.save(f'D:\crush\png\{fileSequence}.png')
+    im1.save(f'D:\crush\png\{fileSequence}.png') #Location of folder to save png files
     fileSequence += 1
 
 if __name__ == "__main__":
